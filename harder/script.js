87d82d1952ce29bf24a3d498ef6a33367abaffc0
Lalon Hossain <36957557+lalonhossain@users.noreply.github.com>
2020-09-03 17:31:15 +0600

    Update script.js

--- harder/script.js
+++ harder/script.js
@@ -16,3 +16,17 @@ function myFunction() {
   	}
  	document.getElementById("demo").innerHTML = text;
 }
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+
+for (var i = 0; i < names.length; i++) {
+
+  var firstLetter = names[i].charAt(0).toLowerCase();
+
+  if (firstLetter === 'j') {
+    console.log("Goodby " + names[i]);
+  } else {
+    console.log("Hello " + names[i]);
+  }
+
+
+}
